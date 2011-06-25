37641fb2557e567281f4837125562028350039cf
fomine <fomine@afccfc53-dfbf-4c9c-a56a-642eddf6e55f>
2011-06-25 09:38:16 +0000

    ...
    git-svn-id: https://svn.code.sf.net/p/turnservernet/code/trunk@9 afccfc53-dfbf-4c9c-a56a-642eddf6e55f

---
+++ Turn.Server/ILogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Turn.Server
+{
+	public interface ILogger
+	{
+		void WriteError(string message);
+		void WriteWarning(string message);
+		void WriteInformation(string message);
+	}
+
+	class NullLogger
+		: ILogger
+	{
+		public void WriteError(string message)
+		{
+		}
+
+		public void WriteWarning(string message)
+		{
+		}
+
+		public void WriteInformation(string message)
+		{
+		}
+	}
+}
